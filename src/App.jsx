800996eb546deda2b0c64d3c4e69cfbf920ef770
NARD <glenardrefamonte1@gmail.com>
2024-04-27 03:25:39 +0800

    Uncomment the categories page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Login from "./components/Login-user/ImageCarousel";
 import SignUp from "./components/Login-user/SIgnUp";
 // import ProductPage from "./components/page/Product_Page";
 // import Categories from "./components/page/Categories";
-import { ProductProviderContext } from "./components/Product/ProductProviderContext";
+import Categories from "./pages/Categories/Categ";
+import { ProductProviderContext } from "./context/ProductProviderContext";
 import { CartProvider } from "./context/CartContext";
 import { FloatButton } from "antd";
 
@@ -24,7 +25,7 @@ function App() {
             />
 
             <Route path="/product-page/" element={<ProductPage />} /> */}
-            {/* <Route path="/categories" element={<Categories />} /> */}
+            <Route path="/categories" element={<Categories />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
           </Routes>
