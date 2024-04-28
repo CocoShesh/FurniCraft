bc365b94e272839f4607d0220d34afb7072141fe
NARD <glenardrefamonte1@gmail.com>
2024-04-29 02:27:53 +0800

    fix the height and the alignment together the reponsiveness

--- src/components/Login-user/ImageCarousel.jsx
+++ src/components/Login-user/ImageCarousel.jsx
@@ -1,45 +1,41 @@
-import React from "react";
-// import SignUp from "./Login";
+import React, { useState } from "react";
 import { Carousel } from "antd";
+import Login from "./Login";
+import SignUp from "./SIgnUp";
 
-const contentStyle = {
-  height: "700px",
-  width: "500px",
-  color: "#fff",
-  lineHeight: "160px",
-  textAlign: "center",
-  background: "#364d79",
-};
-export default function App() {
+export default function ImageCarousel({ children }) {
+  const [isCreateAccount, setIsCreateAccount] = useState(false);
   return (
     <>
-      <section className="flex items-center justify-center my-2 rounded-lg border-2 mx-auto shadow-xl w-[1200px] max-sm:w-full max-sm:flex-col ">
-        <section className="max-sm:hidden">
-          <Carousel autoplay className="w-[700px]  " effect="fade">
-            <div>
-              <img
-                src="https://img.freepik.com/free-photo/delivery-concept-handsome-african-american-delivery-man-rush-running-delivering-package-customer-isolated-grey-studio-background-copy-space_1258-1211.jpg?w=2000"
-                alt=""
-                className="h-[610px]"
-              />
-            </div>
-            <div>
-              <img
-                src="https://img.freepik.com/free-photo/delivery-concept-portrait-happy-african-american-delivery-man-red-cloth-holding-box-package-isolated-grey-studio-background-copy-space_1258-1216.jpg?w=2000"
-                alt=""
-                className="h-[610px]"
-              />
-            </div>
-            <div>
-              <img
-                src="https://img.freepik.com/free-photo/delivery-concept-portrait-happy-african-american-delivery-man-pointing-hand-present-box-package-isolated-grey-studio-background-copy-space_1258-1263.jpg"
-                alt=""
-                className="h-[610px]"
-              />
-            </div>
-          </Carousel>
+      <section className="flex min-h-screen w-full items-center justify-center max-lg:px-20 max-sm:px-3 px-20   ">
+        <section className="flex items-center justify-center my-2  2xl:w-[1300px] rounded-lg  bg-[#e9e9e9] w-full max-xl:w-[500px]  max-sm:w-full max-sm:flex-col ">
+          <section className="max-xl:hidden">
+            <Carousel autoplay className="w-[700px]   " effect="fade">
+              <div>
+                <img
+                  src="https://img.freepik.com/free-photo/delivery-concept-handsome-african-american-delivery-man-rush-running-delivering-package-customer-isolated-grey-studio-background-copy-space_1258-1211.jpg?w=2000"
+                  alt=""
+                  className="h-[620px]   rounded-l-lg "
+                />
+              </div>
+              <div>
+                <img
+                  src="https://img.freepik.com/free-photo/delivery-concept-portrait-happy-african-american-delivery-man-red-cloth-holding-box-package-isolated-grey-studio-background-copy-space_1258-1216.jpg?w=2000"
+                  alt=""
+                  className="h-[620px]   rounded-l-lg"
+                />
+              </div>
+              <div>
+                <img
+                  src="https://img.freepik.com/free-photo/delivery-concept-portrait-happy-african-american-delivery-man-pointing-hand-present-box-package-isolated-grey-studio-background-copy-space_1258-1263.jpg"
+                  alt=""
+                  className="h-[620px]  rounded-l-lg "
+                />
+              </div>
+            </Carousel>
+          </section>
+          <section className="w-full">{children}</section>
         </section>
-        <section className="w-[500px] ">{/* <SignUp /> */}</section>
       </section>
     </>
   );
