b66119afc3c44a5927b3b30e9f1105c10fa327df
NARD <glenardrefamonte1@gmail.com>
2024-05-01 01:50:57 +0800

    Feat: Added data for more images

--- src/components/Product/data.js
+++ src/components/Product/data.js
@@ -1,3 +1,43 @@
+import OtherImgs1 from "/otherProducts/other1.jpg";
+import OtherImgs2 from "/otherProducts/other2.jpg";
+import Toster1 from "/otherProducts/toster1.jpg";
+import Toster2 from "/otherProducts/toster2.jpg";
+import LampBlue1 from "/otherProducts/lampblue1.webp";
+import LampBlue2 from "/otherProducts/lampblue2.webp";
+import PulpUnit1 from "/otherProducts/pulpunit1.jpg";
+import PulpUnit2 from "/otherProducts/pulpunit2.jpg";
+import ModernGold1 from "/otherProducts/moderngold1.webp";
+import ModernGold2 from "/otherProducts/moderngold2.webp";
+import CleaningOil1 from "/otherProducts/cleaningoil1.jpg";
+import CleaningOil2 from "/otherProducts/cleaningoil2.jpg";
+import BlackAndWhite1 from "/otherProducts/blackandwhite1.webp";
+import BlackAndWhite2 from "/otherProducts/blackandwhite2.webp";
+import BodyOil1 from "/otherProducts/bodyoil1.jpg";
+import BodyOil2 from "/otherProducts/bodyoil2.jpg";
+import Shelf1 from "/otherProducts/shelf1.webp";
+import Shelf2 from "/otherProducts/shelf2.webp";
+import Vase1 from "/otherProducts/vase1.webp";
+import Vase2 from "/otherProducts/vase2.webp";
+import ChairBoom1 from "/otherProducts/chairboom1.webp";
+import ChairBoom2 from "/otherProducts/chairboom2.webp";
+import BlackLamp1 from "/otherProducts/blacklamp1.webp";
+import BlackLamp2 from "/otherProducts/blacklamp2.jpg";
+import GoldenLamp1 from "/otherProducts/goldenlamp1.webp";
+import GoldenLamp2 from "/otherProducts/goldenlamp2.webp";
+import Cotton1 from "/otherProducts/cotton1.jpg";
+import Cotton2 from "/otherProducts/cotton2.jpg";
+import WoodChair1 from "/otherProducts/woodchair1.webp";
+import WoodChair2 from "/otherProducts/woodchair2.webp";
+import Copenhagen1 from "/otherProducts/copenhagen1.webp";
+import Copenhagen2 from "/otherProducts/copenhagen2.webp";
+import Cam1 from "/otherProducts/cam1.webp";
+import Cam2 from "/otherProducts/cam2.jpg";
+import TableLamp1 from "/otherProducts/tablelamp1.webp";
+import TableLamp2 from "/otherProducts/tablelamp2.webp";
+import MatLamp1 from "/otherProducts/mat1.webp";
+import MatLamp2 from "/otherProducts/mat2.webp";
+import Comfy1 from "/otherProducts/comfy1.webp";
+import Comfy2 from "/otherProducts/comfy2.webp";
 const products = [
   {
     id: 1,
@@ -7,6 +47,7 @@ const products = [
     imageSrc: "/chair.jpg",
     imageAlt:
       "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
+    otherImgs: ["/chair.jpg", OtherImgs1, OtherImgs2],
     Description:
       "Indulge in the lap of luxury with our exquisite Little Armchair Sheepskin. This opulent armchair is designed to elevate your living space with its sumptuous comfort and timeless elegance. Upholstered in soft and plush sheepskin, it not only offers a cozy retreat but also adds a touch of warmth and sophistication to your decor. The chair's design is a masterpiece of craftsmanship, featuring meticulously detailed stitching and a sturdy frame that ensures durability for years to come. Whether you place it in your living room, bedroom, or study, this armchair is sure to become the focal point of any room. Don't miss the opportunity to experience unparalleled comfort and style with this exceptional piece of furniture.",
     Texture: "Sheep Skin",
@@ -19,6 +60,7 @@ const products = [
     href: "/product-page",
     price: "$45",
     imageSrc: "/bluetooth.jpg",
+    otherImgs: ["/bluetooth.jpg", Toster1, Toster2],
     imageAlt:
       "Olive drab green insulated bottle with flared screw lid and flat top.",
     Description:
@@ -33,6 +75,7 @@ const products = [
     href: "/product-page",
     price: "$79",
     imageSrc: "/bed-lamp.png",
+    otherImgs: ["/bed-lamp.png", LampBlue1, LampBlue2],
     imageAlt:
       "Person using a pen to cross a task off a productivity paper card.",
     Description:
@@ -47,6 +90,7 @@ const products = [
     href: "/product-page",
     price: "$254",
     imageSrc: "/box.jpg",
+    otherImgs: ["/box.jpg", PulpUnit1, PulpUnit2],
     imageAlt:
       "Hand holding black machined steel mechanical pencil with brass tip and top.",
     Description:
@@ -61,6 +105,7 @@ const products = [
     href: "/product-page",
     price: "$189",
     imageSrc: "/glossy-lamp.jpg",
+    otherImgs: ["/glossy-lamp.jpg", ModernGold1, ModernGold2],
     imageAlt:
       "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
     Description:
@@ -75,6 +120,7 @@ const products = [
     href: "/product-page",
     price: "$67",
     imageSrc: "/mousturizer.jpg",
+    otherImgs: ["/mousturizer.jpg", CleaningOil1, CleaningOil2],
     imageAlt:
       "Olive drab green insulated bottle with flared screw lid and flat top.",
     Description:
@@ -89,6 +135,7 @@ const products = [
     href: "/product-page",
     price: "$220",
     imageSrc: "/black-white.jpg",
+    otherImgs: ["/black-white.jpg", BlackAndWhite1, BlackAndWhite2],
     imageAlt:
       "Person using a pen to cross a task off a productivity paper card.",
     Description:
@@ -103,6 +150,7 @@ const products = [
     href: "/product-page",
     price: "$115",
     imageSrc: "/divider.jpg",
+    otherImgs: ["/divider.jpg", Shelf1, Shelf2],
     imageAlt:
       "Hand holding black machined steel mechanical pencil with brass tip and top.",
     Description:
@@ -117,6 +165,7 @@ const products = [
     href: "/product-page",
     price: "$67",
     imageSrc: "/vase.png",
+    otherImgs: ["/vase.png", Vase1, Vase2],
     imageAlt:
       "Olive drab green insulated bottle with flared screw lid and flat top.",
     Description:
@@ -131,6 +180,7 @@ const products = [
     href: "/product-page",
     price: "$220",
     imageSrc: "/chair2.jpg",
+    otherImgs: ["/chair2.jpg", Comfy1, Comfy2],
     imageAlt:
       "Person using a pen to cross a task off a productivity paper card.",
     Description:
@@ -145,6 +195,7 @@ const products = [
     href: "/product-page",
     price: "$115",
     imageSrc: "/comfy-chair2.jpg",
+    otherImgs: ["/comfy-chair2.jpg", ChairBoom1, ChairBoom2],
     imageAlt:
       "Hand holding black machined steel mechanical pencil with brass tip and top.",
     Description:
@@ -159,6 +210,7 @@ const products = [
     href: "/product-page",
     price: "$115",
     imageSrc: "/container.jpg",
+
     imageAlt:
       "Hand holding black machined steel mechanical pencil with brass tip and top.",
     Description:
@@ -173,6 +225,7 @@ const products = [
     href: "/product-page",
     price: "$115",
     imageSrc: "/cleanser.jpg",
+    otherImgs: ["/cleanser.jpg", BodyOil1, BodyOil2],
     imageAlt:
       "Hand holding black machined steel mechanical pencil with brass tip and top.",
     Description:
@@ -187,6 +240,7 @@ const products = [
     href: "/product-page",
     price: "$115",
     imageSrc: "/cctv.jpg",
+    otherImgs: ["/cctv.jpg", Cam1, Cam2],
     imageAlt:
       "Hand holding black machined steel mechanical pencil with brass tip and top.",
     Description:
