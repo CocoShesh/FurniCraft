2c7cd839bc8c24ab589c555a014e61798b23301f
NARD <glenardrefamonte1@gmail.com>
2024-05-01 01:51:22 +0800

    Feat: Added more images then created a function

--- src/pages/Product-page/ProductPage.jsx
+++ src/pages/Product-page/ProductPage.jsx
@@ -16,11 +16,16 @@ function Product_Page() {
   const { addToCart } = useContext(CartContext);
   const { productName } = useParams();
   const [quantity, setQuantity] = useState(1);
+  const [selectedImage, setSelectedImage] = useState("");
 
   const product = useMemo(
     () => data.find(item => item.name === productName),
     [productName]
   );
+
+  useEffect(() => {
+    setSelectedImage(product?.imageSrc);
+  }, [productName]);
 
   useEffect(() => {
     if (product) {
@@ -81,59 +86,85 @@ function Product_Page() {
         <h2 className="text-center text-5xl mb-10 max-sm:text-4xl font-bold text-[#b9b8b8]">
           {product.name}
         </h2>
-        <section className="flex items-center justify-center h-fit w-full  py-10  2xl:w-[1500px] max-lg:px-5 lg:px-16 max-lg:flex-col gap-20 2xl:px-0">
-          <LazyLoadImage
-            effect="blur"
-            wrapperProps={{
-              style: { transitionDelay: "1s" },
-            }}
-            src={product.imageSrc}
-            alt={product.imageAlt}
-            height={300}
-            width={300}
-            className="w-[450px] rounded-lg brightness-90 hover:brightness-100 max-lg:mb-0 lg:mb-20 bg-white "
-          />
-          <section className="w-full">
-            <h1 className="text-2xl font-bold ">Description</h1>
-            <p className="py-2 text-justify text-[#b9b8b893]">
-              {product.Description}
-            </p>
-            <section className="flex  max-sm:flex-wrap items-center gap-10 mt-10">
-              <p className="font-bold text-2xl"> Quantity</p>
-              <section className="flex text-black">
-                <div
-                  className="w-[50px] h-[50px] border-[1px] text-4xl text-center cursor-pointer bg-[#cfcdcd]  border-black "
-                  onClick={() => setQuantity(quantity + 1)}
-                >
-                  &#43;
-                </div>
-                <input
-                  type="text"
-                  readOnly
-                  value={quantity}
-                  className="w-[50px] h-[50px] border-[1px] text-3xl text-center bg-[#cfcdcd]  outline-none  border-black"
-                />
-                <div
-                  className="w-[50px] h-[50px] border-[1px] text-4xl text-center cursor-pointer bg-[#cfcdcd]  border-black"
-                  onClick={() => {
-                    if (quantity > 1) {
-                      setQuantity(quantity - 1);
-                    }
-                  }}
-                >
-                  &#8722;
-                </div>
+        <section className="flex items-center justify-center h-fit w-full   py-10  2xl:w-[1500px] max-lg:px-5 lg:px-16 max-lg:flex-col gap-20 2xl:px-0">
+          <section className="flex flex-col items-center max-lg:w-fit">
+            <LazyLoadImage
+              effect="blur"
+              wrapperProps={{
+                style: { transitionDelay: "1s" },
+              }}
+              src={selectedImage ? selectedImage : product.imageSrc}
+              alt={product.imageAlt}
+              height={300}
+              width={300}
+              className="w-[450px] max-lg:w-full h-[350px] rounded-lg brightness-90 hover:brightness-100 mb-5 bg-white "
+            />
+            <section className="flex gap-3  max-lg:w-fit max-lg:mt-5">
+              {product?.otherImgs.map((img, index) => {
+                return (
+                  <LazyLoadImage
+                    key={index}
+                    effect="blur"
+                    wrapperProps={{
+                      style: { transitionDelay: "1s" },
+                    }}
+                    src={img}
+                    alt={product.imageAlt}
+                    height={300}
+                    width={300}
+                    onClick={() => setSelectedImage(img)}
+                    className="w-[200px]   max-lg:w-[150px] h-[100px] cursor-pointer  rounded-lg brightness-90 hover:brightness-100 max-lg:mb-0 lg:mb-20 bg-white "
+                  />
+                );
+              })}
+            </section>
+          </section>
+
+          <section className="w-full flex flex-col justify-between lg:min-h-[550px] max-lg-full">
+            <section className="flex flex-col justify-between h-[350px] max-lg:h-full ">
+              <section>
+                <h1 className="text-4xl font-bold ">Description</h1>
+                <p className="py-2 text-justify mt-3 text-[#b9b8b893] lg:text-xl">
+                  {product.Description}
+                </p>
               </section>
-              <p className="font-bold text-2xl">${totalAmount}</p>
+              <section className="flex  max-sm:flex-wrap items-center gap-10 mt-10">
+                <p className="font-bold text-2xl"> Quantity</p>
+                <section className="flex text-black">
+                  <div
+                    className="w-[50px] h-[50px] border-[1px] text-4xl text-center cursor-pointer bg-[#cfcdcd]  border-black "
+                    onClick={() => setQuantity(quantity + 1)}
+                  >
+                    &#43;
+                  </div>
+                  <input
+                    type="text"
+                    readOnly
+                    value={quantity}
+                    className="w-[50px] h-[50px] border-[1px] text-3xl text-center bg-[#cfcdcd]  outline-none  border-black"
+                  />
+                  <div
+                    className="w-[50px] h-[50px] border-[1px] text-4xl text-center cursor-pointer bg-[#cfcdcd]  border-black"
+                    onClick={() => {
+                      if (quantity > 1) {
+                        setQuantity(quantity - 1);
+                      }
+                    }}
+                  >
+                    &#8722;
+                  </div>
+                </section>
+                <p className="font-bold text-2xl">${totalAmount}</p>
+              </section>
             </section>
             <section className="flex  max-sm:flex-col mt-10 justify-end gap-5">
               <button
-                className="w-[200px]  max-sm:w-full h-[50px] border-[1px] border-black font-bold text-2xl hover:bg-[#cfcdcd] hover:text-black rounded-md"
+                className="w-[200px]  max-sm:w-full h-[50px] border-[1px] border-black font-bold text-2xl hover:bg-[#cfcdcd] hover:text-black transform hover:scale-105 transition-all duration-300 ease-in-out"
                 onClick={handleAddToCart}
               >
                 Add To Cart
               </button>
-              <button className="w-[200px] max-sm:w-full   h-[50px] border-[1px] border-black font-bold text-2xl text-black bg-[#f7cd7c] hover:bg-transparent  hover:text-[#f7cd7c] rounded-md">
+              <button className="w-[200px] max-sm:w-full   h-[50px] border-[1px] border-black font-bold text-2xl text-black bg-[#f7cd7c] hover:bg-transparent  hover:text-[#f7cd7c] transform hover:scale-105 transition-all duration-300 ease-in-out">
                 Buy Now
               </button>
             </section>
